68b7c4e7324e0bd82da2473a1ceb50e28d2afa49
Durai <t.cheirmadurai@gmail.com>
2026-06-23 16:10:44 +0530

    added logic for recipe image upload.

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -37,7 +37,7 @@ class RecipeSerializer(serializers.ModelSerializer):
         model = Recipe
         fields = [
             'id', 'title', 'time_minutes', 'price', 'link', 'tags',
-            'ingredients',
+            'ingredients', 'image',
         ]
         read_only_fields = ['id']
 
@@ -97,3 +97,12 @@ class RecipeDetailSerializer(RecipeSerializer):
     class Meta(RecipeSerializer.Meta):
         fields = RecipeSerializer.Meta.fields + ['description']
 
+
+class RecipeImageSerializer(serializers.ModelSerializer):
+    """Serializer for uploading images to recipes."""
+
+    class Meta:
+        model = Recipe
+        fields = ['id', 'image']
+        read_only_fields = ['id']
+        extra_kwargs = {'image': {'required': 'True'}}
